24042f31228282cdbd144023d764ec0ed7974c62
Your Name <you@example.com>
2025-08-07 22:32:43 +0300

    New update, adde JS and new slide

---
+++ index.js
@@ -0,0 +1,29 @@
+const like1 = document.getElementById('img__like1')
+
+like1.addEventListener('click',() => {
+    like1.src = 'images/like-active.png';
+})
+
+like1.addEventListener('dblclick',() => {
+    like1.src = 'images/like.png';
+})
+
+const like2 = document.getElementById('img__like2')
+
+like2.addEventListener('click',() => {
+    like2.src = 'images/like-active.png';
+})
+
+like2.addEventListener('dblclick',() => {
+    like2.src = 'images/like.png';
+})
+
+const like3 = document.getElementById('img__like3')
+
+like3.addEventListener('click',() => {
+    like3.src = 'images/like-active.png';
+})
+
+like3.addEventListener('dblclick',() => {
+    like3.src = 'images/like.png';
+})
