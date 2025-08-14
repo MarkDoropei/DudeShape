45657951865098c51f9c41a4085585eb4cc801d2
Your Name <you@example.com>
2025-08-14 17:57:09 +0300

    Added swiper... yes

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const like1 = document.getElementById('img__like1')
+const like = document.getElementsByClassName('img__like')
 
-like1.addEventListener('click',() => {
-    like1.src = 'images/like-active.png';
+like.addEventListener('click',() => {
+    like.src = 'images/like-active.png';
 })
 
-like1.addEventListener('dblclick',() => {
-    like1.src = 'images/like.png';
-})
-
-const like2 = document.getElementById('img__like2')
-
-like2.addEventListener('click',() => {
-    like2.src = 'images/like-active.png';
-})
-
-like2.addEventListener('dblclick',() => {
-    like2.src = 'images/like.png';
-})
-
-const like3 = document.getElementById('img__like3')
-
-like3.addEventListener('click',() => {
-    like3.src = 'images/like-active.png';
-})
-
-like3.addEventListener('dblclick',() => {
-    like3.src = 'images/like.png';
+like.addEventListener('dblclick',() => {
+    like.src = 'images/like.png';
 })
