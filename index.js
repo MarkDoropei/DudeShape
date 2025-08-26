f317edcd62238c7649a923a9b83a208255f87511
Your Name <you@example.com>
2025-08-26 20:33:05 +0300

    Almost Final update

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const like = document.getElementsByClassName('img__like')
-like.addEventListener('click',() => {
-    like.src = 'images/like-active.png';
-})
+// const like = document.getElementsByClassName('img__like')
+// like.addEventListener('click',() => {
+//     like.src = 'images/like-active.png';
+// })
 
-like.addEventListener('dblclick',() => {
-    like.src = 'images/like.png';
-})
+// like.addEventListener('dblclick',() => {
+//     like.src = 'images/like.png';
+// })
+
+const text = document.querySelectorAll('.features__item')
+
+text.forEach(item => {
+  item.addEventListener('click', () => {
+    text.forEach(i => i.style.color = '');
+    text.forEach(i => i.style.fontWeight = 300); 
+    text.forEach(i => i.style.borderBottom = 'none');
+    item.style.color = '#154444';
+    item.style.fontWeight = 900;
+    item.style.borderBottom = '1.5px solid #154444'
+  });
+});
